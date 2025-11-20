4e881264b038eaf16461df30cdb959a0a4094864
Chirag-gowdaa <155340544+Chirag-gowdaa@users.noreply.github.com>
2025-11-20 10:23:44 +0530

    Update greeting message in HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, DevOps CI/CD using GitHub Actions!");
+        System.out.println("Hello, DevOps CI/CD using GitHub Actions! 2nd trial");
 
         System.out.println("Change #1: Verifying automatic workflow trigger on push.");
 
